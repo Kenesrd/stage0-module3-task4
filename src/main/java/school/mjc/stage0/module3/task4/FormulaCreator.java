9cb4bd89fb87eeafab06ef250ebea0229a428e10
kenes <kenesvip@gmail.com>
2023-08-23 22:20:42 +0500

    module 3 task 4 done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+//        I don't know how to solve the last task because this don't have explanation or example
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+
+        String s = String.valueOf((char)c);
+        s += String.valueOf((char)h);
+        s += String.valueOf((char)a);
+        s += String.valueOf((char)r);
+
+        System.out.println(s);
     }
 }
